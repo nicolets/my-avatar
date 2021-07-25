890e1a8dcec6980891126d10de8f5921c6066013
Nicole <nicole30401@gmail.com>
2021-07-25 18:32:23 +0300

    add avatar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
+import Avatar from './Avatar/Avatar';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Avatar url="https://scontent.ftlv1-1.fna.fbcdn.net/v/t31.18172-8/24131711_10214894748865428_4401002959166506828_o.jpg?_nc_cat=103&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=bShUgbPQTGUAX9QR-sk&_nc_ht=scontent.ftlv1-1.fna&oh=dc20d8a86e1142d31af9a6a17913e324&oe=612488F3" />
     </div>
   );
 }
---
+++ src/Avatar/Avatar.js
@@ -0,0 +1,5 @@
+import './Avatar.css'
+
+export default function Avatar(props) {
+    return <img src={props.url} className="myAvatar" /> 
+}
